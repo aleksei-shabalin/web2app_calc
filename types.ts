b7a85f76579b88e9b53513985221fc4744844049
Aleksei <sevennder@gmail.com>
2025-11-30 20:05:37 +0400

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,12 @@
+export interface FunnelMetrics {
+  targetCPA: number;
+  conversionRate: number;
+  maxCPC: number;
+}
+
+export interface SimulationData {
+  clicks: number;
+  cost: number;
+  conversions: number;
+  cpa: number;
+}
